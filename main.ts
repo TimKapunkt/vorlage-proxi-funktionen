de7020bb45a262e3297783d97a23df2ccc9de1f4
TimKapunkt <96684814+TimKapunkt@users.noreply.github.com>
2022-07-29 18:15:41 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,24 @@
-input.onButtonPressed(Button.A, function () {
-	
+let IR = 0
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        IR = Proxi.Lese_RBlock()
+    } else if (input.buttonIsPressed(Button.B)) {
+        IR = Proxi.Lese_LBlock()
+    } else if (Proxi.RBlock(264)) {
+        basic.showLeds(`
+            . # . . .
+            . # . . .
+            . # . . .
+            . # . . .
+            . # # # .
+            `)
+    } else if (Proxi.LBlock(264)) {
+        basic.showLeds(`
+            . # # # .
+            . # . # .
+            . # # # .
+            . # # . .
+            . # . # .
+            `)
+    }
 })
-for (let index = 0; index < 3; index++) {
-    basic.showIcon(IconNames.Diamond)
-    basic.showIcon(IconNames.SmallDiamond)
-}
